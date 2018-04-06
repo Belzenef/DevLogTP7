39edb877b22d7df7e937048e701b58694f5590d5
Elise Jacquemet <elise.jacquemet@insa-lyon.fr>
2018-04-06 14:47:43 +0200

    Deleted useless comments

--- string.cpp
+++ string.cpp
@@ -8,40 +8,30 @@
 
   // Constructors
   string::string(const char* s) {  
-    /*From c-string constructor :
-    Copies the character sequence pointed by the given argument (s)
-    INPUT:
-      const char* s: Pointer to a null-terminated sequence of characters.
-    OUTPUT:
-      none
-    PRECONDITIONS:
-      s is null-terminated and is not nullptr
-    POSTCONDITIONS:
-      none
-    EXCEPTION SAFETY:
-      There are no effects in case an exception is thrown (strong guarantee). 
-      If the resulting string length would exceed the max_size, a length_error 
-      exception is thrown.
-      A bad_alloc exception is thrown if the function needs to allocate 
-      storage and fails. */
-      
+    //precondition : s is null-terminated and is not nullptr
+    
+    //finding size of cstr
     int csize=0;
-    while(s[csize] != '\0'){ // finding size of cstr
+    while(s[csize] != '\0'){ 
       ++csize;
     }
     ++csize;
     
+    //controlling that the size would not exceed MAX_SIZE
     if(csize>MAX_SIZE){
       throw std::length_error("size of c-string is greater than MAX_SIZE\n");
     }
     
+    //copies the character sequence pointed by the given argument (s)
     try{ 
       pointer_ = new char[csize];
-      for(int i=0; i<csize; ++i){
+      for(int i=0; i<csize; ++i){ 
         pointer_[i]=s[i];
       }
-      size_=csize-1; // same size than cstr without null character '\0'
+      size_=csize-1; //same size than cstr without null character '\0'
       reserve(csize);
+      
+    //catch exceptions
     }catch (const std::length_error& le){
       throw std::length_error(le.what());
     }catch (const std::bad_alloc& ba){
@@ -171,23 +161,9 @@
   }
 
   string& string::operator=(char c){
-  /*Assigns a new value to the string, replacing its current contents to 
-   a single copy of the parameter given.
-    INPUT:
-      char c: a character, copied as the new value for the string.
-    OUTPUT:
-      *this
-    PRECONDITIONS:
-      c is not null
-    POSTCONDITIONS:
-      The object is modified: the string has been assigned a new value
-    EXCEPTION SAFETY:
-      There are no effects in case an exception is thrown (strong guarantee). 
-      If the resulting string length would exceed the max_size, a length_error 
-      exception is thrown.
-      A bad_alloc exception is thrown if the function needs to allocate 
-      storage and fails. */
+  //Precondition : c is not null
 
+    //copying c into this.pointer_ :
 	  try{
       delete[] pointer_;
       char* ptr = new char[2];
@@ -198,6 +174,7 @@
       reserve(2);
       return(*this);
       
+    //catch exceptions
     }catch (const std::length_error& le){
       throw std::length_error(le.what());
     }catch (const std::bad_alloc& ba){
@@ -222,60 +199,15 @@
 		return size_;}
 
   size_t string::length() const noexcept{
-    /*Returns the length of the string, in terms of bytes, from the first
-    character to the first null-character encountered (excluded).  
-    INPUT:
-      none
-    OUTPUT:
-      size_t size_ : the value corresponding to the number of bytes in the string.
-    PRECONDITIONS:
-      none
-    POSTCONDITIONS:
-      none
-    EXCEPTION SAFETY:
-      No-throw guarantee: this member function never throws exceptions.*/
-
+    //Synonym of size()
     return size_;}
     
   size_t string::max_size() const noexcept{
-    /*Returns the maximum potential length the string can reach 
-    due the limitation implemented as MAX_SIZE.
-    INPUT:
-      none
-    OUTPUT:
-      size_t MAX_SIZE : the value corresponding to the maximum length the string can reach.
-    PRECONDITIONS:
-      none
-    POSTCONDITIONS:
-      The object is not guaranteed to be able to reach that length: 
-      it can still fail to allocate storage at any point before that length is reached. 
-    EXCEPTION SAFETY:
-      No-throw guarantee: this member function never throws exceptions.*/
-      
     return MAX_SIZE;}
 
   void string::resize(size_t n, char c){
-    /*Resizes the string to a length of n characters.
-    If n is smaller than the current string length, the current value is 
-    shortened to its first n character, removing the characters beyond the nth.
-    If n is greater than the current string length, the current content is 
-    extended by inserting at the end as many characters as needed to reach 
-    a size of n. If c is specified, the new elements are initialized as copies of c, 
-    otherwise, they are initialized as spaces (default value).
-    INPUT:
-      size_t n : the new string length, expressed in number of characters (unsigned)
-      char c : (optional) a character used to fill in the spaces added to the string, if it is to be expanded.
-    OUTPUT:
-      none
-    PRECONDITIONS:
-      none
-    POSTCONDITIONS:
-      The object is modified: the string has been assigned a new value
-    EXCEPTION SAFETY:
-      There are no effects in case an exception is thrown (strong guarantee). 
-      If n is greater than max_size, a length_error exception is thrown.
-      A bad_alloc exception is thrown if the function needs to allocate storage and fails.*/
       
+    //controlling that the size would not exceed MAX_SIZE
     if(n>MAX_SIZE){
       throw std::length_error("requested size is greater than MAX_SIZE\n");
     }
@@ -306,6 +238,8 @@
         size_=n;
         reserve(n+1);
       }
+      
+    //catch exceptions
     }catch (const std::length_error& le){
       throw std::length_error(le.what());
     }catch (const std::bad_alloc& ba){
@@ -435,24 +369,6 @@
 //----------------------------------------------------------------------
   // operator+
 
-/*Returns a newly constructed string object with its value being the 
- concatenation of the characters in lhs followed by those of rhs.
-    INPUT:
-      lhs, rhs: Arguments to the left- and right-hand side of the operator, respectively. If of type char*, it shall point to a null-terminated character sequence.
-    OUTPUT:
-      string result: A string whose value is the concatenation of lhs and rhs, in that order.
-    PRECONDITIONS:
-      if an input is a char*, it is null-terminated, and is not nullptr.
-    POSTCONDITIONS:
-      The object is unmodified.
-    EXCEPTION SAFETY:
-      If an exception is thrown, there are no changes in either string objects.
-      If the resulting string length would exceed the max_size, a length_error 
-      exception is thrown.
-      A bad_alloc exception is thrown if the function needs to allocate 
-      storage and fails.
-*/
-
   string operator+ (const string& lhs, const string& rhs){
     try{
       string result=string(lhs);
@@ -473,19 +389,26 @@
   string operator+(const string& lhs, const char* rhs){    
     try{
       string result(lhs);
+      
       // finding size of rhs
       int s=0;
       while(rhs[s] != '\0'){
         ++s;
       }
       ++s;
+      
+      //finding size of lhs
       size_t total_size = lhs.size_ + s -1;
       result.reserve(total_size+1);
+      
+      //concatenation of rhs into result
       for(int i=0; i<s; ++i){
         result.pointer_[i+lhs.size_]=rhs[i];
       }
       result.size_=total_size;
       return(result);
+      
+    //catch exceptions
     }catch (const std::length_error& le){
       throw std::length_error(le.what());
     }catch (const std::bad_alloc& ba){
@@ -496,19 +419,26 @@
   string operator+(const char* lhs, const string& rhs){ 
     try{   
       string result(lhs);
-      // finding size of lhs
+      
+      //finding size of lhs
       int csize=0;
       while(lhs[csize] != '\0'){
         ++csize;
       }
       ++csize;
+      
+      //reserve storage necessary
       size_t total_size = rhs.size_ + csize -1;
       result.reserve(total_size+1);
+      
+      //concatenation of rhs into result
       for(int i=0; i<csize; ++i){
         result.pointer_[i+csize-1]=rhs.pointer_[i];
       }
       result.size_=total_size;
       return(result);
+      
+    //catch exceptions
     }catch (const std::length_error& le){
       throw std::length_error(le.what());
     }catch (const std::bad_alloc& ba){
